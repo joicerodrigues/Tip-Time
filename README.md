743aa9f4f2eb5cf347ff5d2cb16ae476a145b690
Joice <joicerodrigues020@gmail.com>
2022-08-01 18:18:53 -0300

    Alteração do readme e atualização no design do APP

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
+<div align=center>
+
 # Tip-Time
+
+
+
+
+## 📁 Acesso ao projeto
+
+### Você pode [acessar o código fonte do projeto](https://github.com/joicerodrigues/Tip-Time/blob/28ff97ffa621907bad2e62a89de978f1f39216f9/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt) ou [baixá-lo](https://github.com/joicerodrigues/Tip-Time/archive/refs/heads/main.zip) 
+
+ ### 1 - Clone este repositório utilizando o comando
+#### git clone https://github.com/joicerodrigues/Tip-Time.git
+<br>
+
+## Tecnologias e Ferramentas
+
+<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kotlin/kotlin-original.svg" width=50px heigh=50px />
+<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/androidstudio/androidstudio-original.svg" width=50px heigh=50px />
+
+         
+</div>
--- TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.tiptime
 
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
@@ -16,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.calculateButton.setOnClickListener { calculateTip() }
+
     }
 
     private fun calculateTip() {
@@ -39,5 +44,15 @@ class MainActivity : AppCompatActivity() {
 
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
